aa6c6ae80d8c9ecfef8b096198268436a6559038
Diego Henrique <diegohrp20@gmail.com>
2018-10-31 20:57:07 -0300

    atualização

--- funcoes.js
+++ funcoes.js
@@ -10,6 +10,25 @@ $(document).ready(function(){
            // Apresentar o resultado
            $('#resultado').val(imc);
 
+     if (imc < 18.5) {
+     	document.dados.classificacao.value = "Subnutrição";
+     }
+	else if(imc >= 18.5 && imc <= 24.9){
+		document.dados.classificacao.value = "Peso Saudável";
+	}
+	else if(imc >= 25 && imc <= 29.9){
+		document.dados.classificacao.value = "Sobrepeso";
+	}
+	else if(imc >= 30 && imc <= 34.9){
+		document.dados.classificacao.value = "Obesidade grau 1";
+	}
+	else if(imc >= 35 && imc <= 39.9){
+		document.dados.classificacao.value = "Obesidade grau 2";
+	}
+	else if(imc >= 40){
+		document.dados.classificacao.value = "Obesidade grau 3";
+	}
+	
 	});
  
 });
